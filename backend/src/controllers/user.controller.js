5711f4a4c5eb30892ff692d7b23ab13e8115522f
Hemil-Hansora <hemilhansora1729@gmail.com>
2025-07-12 10:12:13 +0530

    feat: add cookie-parser dependency and implement user registration controller

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+import { ApiError } from "../utils/apiError";
+import { ApiResponse } from "../utils/apiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+
+const register = asyncHandler(async ()=>{
+    
+})
