0cf088c3b64c139dd019c4fbd3df96a2ca8e3a51
kaustav3071 <156155730+kaustav3071@users.noreply.github.com>
2025-07-12 11:12:08 +0530

    feat: update user model and controller for improved registration handling and add new dependencies

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,8 @@
-import User from "../models/User.model.js";
-import { ApiError } from "../utils/apiError";
-import { ApiResponse } from "../utils/apiResponse";
-import { asyncHandler } from "../utils/asyncHandler";
-import { registerSchema } from "../validation";
+import User from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { registerSchema } from "../validation/index.js";
 
 const register = asyncHandler(async (req, res) => {
     const validUser = registerSchema.safeParse(req.body)
@@ -10,7 +10,7 @@ const register = asyncHandler(async (req, res) => {
         throw new ApiError(400 , validUser.error.message)
     }
 
-    const {username , email , password , role } = validUser
+    const {username , email , password , role } = validUser.data
 
     const  user = await User.create({
         password,
@@ -25,3 +25,5 @@ const register = asyncHandler(async (req, res) => {
 
     return res.status(200).json(new ApiResponse(200 ,user , "User created successfully" ))
 })
+
+export { register };
